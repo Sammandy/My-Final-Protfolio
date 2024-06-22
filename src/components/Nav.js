da3587596c6ce4ed33a97a4837724517fddcf8b4
Sonali Banajre <sonalibanjare25@gmail.com>
2024-06-23 02:59:26 +0530

    nav px-5 to px-6

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ const Nav = () => {
   return (
     <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
       <div className='container mx-auto'>
-        <div className='w-full bg-black/20 h-[60px] backdrop-blur-2xl rounded-full max-w-[360px] mx-auto px-5 flex justify-between text-xl text-white/50 items-center'>
+        <div className='w-full bg-black/20 h-[60px] backdrop-blur-2xl rounded-full max-w-[360px] mx-auto px-6 flex justify-between text-xl text-white/50 items-center'>
           <Link
             to='home'
             activeClass='active'
